a4e0d76b73cbb62a469a72449c705008a2b52b33
eleroux46 <eloise.leroux@ensae.fr>
2024-05-02 16:43:01 +0200

    début investigation série

--- Data/reading_data.R
+++ Data/reading_data.R
@@ -1,5 +1,5 @@
 
-fichier<-"~/Projet-de-series-temporelles/Data/valeurs_mensuelles.csv"
+fichier<-"~/2A ENSAE/S2/série tempo linéaires/Projet-de-series-temporelles/Data/valeurs_mensuelles.csv"
 data <- read.csv2(fichier)
 
 colnames(data) <- c("lib", "indice","codes")
