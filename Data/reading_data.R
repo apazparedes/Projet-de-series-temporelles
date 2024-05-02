ee9e85a887dd4645d8b35b56d0164be5264f6f0b
eleroux46 <eloise.leroux@ensae.fr>
2024-05-02 17:50:20 +0200

    merge

--- Data/reading_data.R
+++
@@ -1,6 +0,0 @@
-
-fichier<-"~/2A ENSAE/S2/série tempo linéaires/Projet-de-series-temporelles/Data/valeurs_mensuelles.csv"
-data <- read.csv2(fichier)
-
-colnames(data) <- c("lib", "indice","codes")
-data<-subset(data,codes!="")
